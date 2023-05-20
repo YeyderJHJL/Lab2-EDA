e9c2b8955de85b19d992b1c85a8eb831cdc183b7
PaoMarroquin <pmarroquinpi@unsa.edu.pe>
2023-05-20 17:53:49 -0500

    se añadio el metodo get

--- Ejercicio4/NodeList.java
+++ Ejercicio4/NodeList.java
@@ -2,4 +2,15 @@ package Ejercicio4;
 
 public class NodeList<T> {
   Node<T> root;
+
+
+//Este metodo devuelve el elemento en la posición especificada en la lista.
+   public T get(int index) { 		    //recibe el indice como parametro
+        if (index < 0 || index >= size) {   //informa en caso el indice esta fuera del rango 
+            System.out.print("El índice ingresado está fuera del rango");
+        }
+        return (T) elements[index];         //devuelve el elemento en la posicion especificada
+    } 
+
 }
+
