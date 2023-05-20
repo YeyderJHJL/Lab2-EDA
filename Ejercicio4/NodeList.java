b38a6f8ef972d1e9080ffb7be14f7348560d7c24
PaoMarroquin <pmarroquinpi@unsa.edu.pe>
2023-05-20 18:49:40 -0500

    se añadio el metodo subList

--- Ejercicio4/NodeList.java
+++ Ejercicio4/NodeList.java
@@ -10,8 +10,8 @@ public class NodeList<T> {
             throw new IndexOutOfBoundsException("El índice se encuentra fuera de rango");
         }
         try {
-	   T previousElement = (T) list[index]; //list es el nombre del arreglo
-           list[index] = element;
+	   T previousElement = (T) lista[index]; //lista es el nombre del arreglo
+           lista[index] = element;
          return previousElement;
 	}
 	//si la operación de agregar no es compatible con esta lista
@@ -34,12 +34,25 @@ public class NodeList<T> {
 
 //Este metodo devuelve el número de elementos de esta lista.
    public static <T> int size() {
-	   private List<T> lista; //se usa una lista interna.
+	   private List<T> lista; //lista es el nombre del arreglo.
         return lista.size();  // Utiliza el método size() de la clase List para obtener el tamaño.
 
     }
 
-
-
+//Este metodo devuelve una vista de la parte de esta lista entre fromIndex , inclusive, y toIndex , exclusivo. 
+  public List<T> sublist(int fromIndex, int toIndex) {
+        // verifica si los indices son validos. 
+	if (fromIndex < 0 || toIndex > lista.size() || fromIndex > toIndex) {
+            throw new IndexOutOfBoundsException("Índices de sublista no son validos");
+        }
+        // Crear una nueva lista para almacenar la sublista
+        List<T> subList = new ArrayList<>();
+	// Iterar sobre los elementos desde fromIndex hasta toIndex y los agrega a la sublista
+        for (int i = fromIndex; i < toIndex; i++) {
+            subList.add(elements.get(i));
+        }
+        // Devuelve la sublista creada
+        return subList;
+    }
 }
 
