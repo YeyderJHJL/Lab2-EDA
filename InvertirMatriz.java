8c6b2d77d9f63f7ec965e4ed367d7ba072943861
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-19 23:57:14 -0500

    Se agrego el metodo invertirMatriz() y se añadio la prueba de ambas matrizes para comprobar la inversion

--- InvertirMatriz.java
+++ InvertirMatriz.java
@@ -1,13 +1,26 @@
 public class InvertirMatriz {
   public static void main(String[] args) {
-    int[] matrizPrueba = {4, 3, 6, 0};
-    
+    int[] matrizPrueba = { 4, 3, 6, 0 };
+
     imprimirMatriz(matrizPrueba);
+    imprimirMatriz(invertirMatriz(matrizPrueba));
+  }
+
+  public static int[] invertirMatriz(int[] matriz) {
+    int[] matrizInvertida = new int[matriz.length];
+
+    for (int i = 0; i < matriz.length; i++) {
+      matrizInvertida[matriz.length - 1 - i] = matriz[i];
+    }
+
+    return matrizInvertida;
   }
 
   public static void imprimirMatriz(int[] matriz) {
-    for(int i = 0; i < matriz.length; i++) {
+    for (int i = 0; i < matriz.length; i++) {
       System.out.print(matriz[i] + " ");
     }
+
+    System.out.println();
   }
 }
