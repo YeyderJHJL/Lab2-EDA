7be56c1ff8f4f1f7ac11c7567164aab6141ffbea
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-20 14:14:53 -0500

    Se agrego el metodo add() a NodeList

--- Ejercicio4/NodeList.java
+++ Ejercicio4/NodeList.java
@@ -2,4 +2,21 @@ package Ejercicio4;
 
 public class NodeList<T> {
   Node<T> root;
+
+  public void add(T element) {
+    Node<T> newNode = new Node<>();
+    newNode.setData(element);
+
+    if (this.root == null) {
+      this.root = newNode;
+    } else {
+      Node<T> currentNode = root;
+
+      while (currentNode.nextNode != null) {
+        currentNode = currentNode.nextNode;
+      }
+
+      currentNode.nextNode = newNode;
+    }
+  }
 }
