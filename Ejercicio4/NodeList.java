cd21a8b584a2a7632e3411c1afd2be549597caf4
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-20 14:21:35 -0500

    Ahora si se cargan los comentarios

--- Ejercicio4/NodeList.java
+++ Ejercicio4/NodeList.java
@@ -4,18 +4,26 @@ public class NodeList<T> {
   Node<T> root;
 
   public void add(T element) {
+    // Se crea un nuevo nodo y se le asigna el valor del objeto introducido
     Node<T> newNode = new Node<>();
     newNode.setData(element);
 
+    // Se revisa si la lista esta vacia
     if (this.root == null) {
+      // Como esta vacia, el nodo creado se asigna como el nodo raiz
       this.root = newNode;
     } else {
+      // Creamos la variable currentNode para recorrer la lista
       Node<T> currentNode = root;
 
+      // Este while hace avanzar a currentNode hasta que este ya no tenga un siguiente
+      // nodo lo que significa que este es el ultimo nodo de la lista
       while (currentNode.getNextNode() != null) {
         currentNode = currentNode.getNextNode();
       }
 
+      // Estando en el ultimo nodo de la lista le asignamos el nuevo nodo como el nodo
+      // que le sige. Asi posicionando el nuevo nodo como el nuevo ultimo
       currentNode.setNextNode(newNode);
     }
   }
