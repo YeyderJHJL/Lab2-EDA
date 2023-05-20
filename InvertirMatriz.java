bcaaa18f68a2dd3938770e75bde3a7a2619273b9
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-19 23:51:23 -0500

    Se agrego un metodo para imprimir la Matriz

--- InvertirMatriz.java
+++ InvertirMatriz.java
@@ -1,5 +1,13 @@
 public class InvertirMatriz {
   public static void main(String[] args) {
     int[] matrizPrueba = {4, 3, 6, 0};
+    
+    imprimirMatriz(matrizPrueba);
+  }
+
+  public static void imprimirMatriz(int[] matriz) {
+    for(int i = 0; i < matriz.length; i++) {
+      System.out.print(matriz[i] + " ");
+    }
   }
 }
