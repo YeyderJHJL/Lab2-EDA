2d49d3d82a5337fabd15537b37157956fcc3a403
Jeanpier Valera <jvalera@unsa.edu.pe>
2023-05-21 18:53:01 -0500

    Se agrega listaAnimal y se añade elementos a la lista

--- Ejercicio4/PruebaE4.java
+++ Ejercicio4/PruebaE4.java
@@ -2,6 +2,15 @@ package Ejercicio4;
 
 public class PruebaE4 {
   public static void main(String[] args) {
+      NodeList<String> listaAnimal = new NodeList<>();
 
+      listaAnimal.add("PERRO");
+      listaAnimal.add("GATO");
+      listaAnimal.add("ELEFANTE");
+      listaAnimal.add("CANGURO");
+      listaAnimal.add("SERPIENTE");
+      listaAnimal.add("RATON");
+
+      
   }
 }
